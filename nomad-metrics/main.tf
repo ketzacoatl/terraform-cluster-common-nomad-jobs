352562419db302c7987717f44a618490c1e22e2e
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2018-10-08 05:23:14 -0400

    nomad-metrics: add `nomad_address` variable and include in job template

--- nomad-metrics/main.tf
+++ nomad-metrics/main.tf
@@ -3,13 +3,14 @@ data "template_file" "nomad-metrics" {
   template = "${file("${path.module}/templates/nomad-metrics.hcl")}"
 
   vars {
-    job_name    = "${var.job_name}"
-    region      = "${var.region}"
-    datacenters = "${join(",",var.datacenters)}"
-    node_class  = "${var.node_class}"
-    cpu_limit   = "${var.cpu_limit}"
-    mem_limit   = "${var.mem_limit}"
-    net_limit   = "${var.net_limit}"
+    job_name      = "${var.job_name}"
+    region        = "${var.region}"
+    datacenters   = "${join(",",var.datacenters)}"
+    node_class    = "${var.node_class}"
+    cpu_limit     = "${var.cpu_limit}"
+    mem_limit     = "${var.mem_limit}"
+    net_limit     = "${var.net_limit}"
+    nomad_address = "${var.nomad_address}"
   }
 }
 
