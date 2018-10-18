c6c6ca5b32133379a78f67695416656a4c56c270
Paul Montero <lpaulmp@gmail.com>
2018-10-18 07:58:41 -0400

    fabiolb: Fix count variable

--- fabio/main.tf
+++ fabio/main.tf
@@ -1,18 +1,18 @@
 data "template_file" "fabio" {
-  count    = "${var.run}"
+  count    = "${var.run == "true" ? 1 : 0}"
   template = "${file("${path.module}/templates/fabio.hcl")}"
 
   vars {
-    job_name    = "${var.job_name}"
-    region      = "${var.region}"
-    datacenters = "${join(",",var.datacenters)}"
-    version     = "${var.version}"
-    checksum    = "${var.checksum}"
-    node_class  = "${var.node_class}"
-    cpu_limit   = "${var.cpu_limit}"
-    mem_limit   = "${var.mem_limit}"
-    net_limit   = "${var.net_limit}"
-    consul_address = "${consul_address}"
+    job_name       = "${var.job_name}"
+    region         = "${var.region}"
+    datacenters    = "${join(",",var.datacenters)}"
+    version        = "${var.version}"
+    checksum       = "${var.checksum}"
+    node_class     = "${var.node_class}"
+    cpu_limit      = "${var.cpu_limit}"
+    mem_limit      = "${var.mem_limit}"
+    net_limit      = "${var.net_limit}"
+    consul_address = "${var.consul_address}"
   }
 }
 
